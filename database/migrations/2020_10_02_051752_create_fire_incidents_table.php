7afae78381c7fed24a417daf36004f0f524d69b9
Hilary and Garth <hilarysoong@gmail.com>
2020-10-02 13:58:33 +0800

    env file updated, database created, fire_incident table created

---
+++ database/migrations/2020_10_02_051752_create_fire_incidents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFireIncidentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fire_incidents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',10);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone_number',10);
+            $table->longText('Message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fire_incidents');
+    }
+}
